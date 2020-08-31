05c14e2495c715e37c1967db66b66e21e29f339f
ClaytonC <cjcanady1113@gmail.com>
2020-08-31 19:59:06 -0400

    added links to images

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -7,20 +7,28 @@ class Portfolio extends Component {
 				<h3>My Projects</h3>
 				<div className='container'>
 					<div className='project'>
-						<img
-							src={require('./images/game-of-thrones.png')}
-							alt='game of thrones quiz'
-						/>
+						<a
+							href='https://claytoncanady.github.io/Game-of-Thrones-Quiz/'
+							target='blank'>
+							<img
+								src={require('./images/game-of-thrones.png')}
+								alt='game of thrones quiz'
+							/>
+						</a>
 						<p className='desc'>
 							A quiz game based on the hit show Game of Thrones. Features
 							include high score tracking.
 						</p>
 					</div>
 					<div className='project'>
-						<img
-							src={require('./images/D&D.png')}
-							alt='Dungeons and Dragons application'
-						/>
+						<a
+							href='https://dungeons-and-dragons-info.herokuapp.com/'
+							target='blank'>
+							<img
+								src={require('./images/D&D.png')}
+								alt='Dungeons and Dragons application'
+							/>
+						</a>
 						<p className='desc'>
 							An application that accesses an external api to display
 							information on Dungeons and Dragons. All lists returned are
@@ -28,10 +36,14 @@ class Portfolio extends Component {
 						</p>
 					</div>
 					<div className='project'>
-						<img
-							src={require('./images/fleet.png')}
-							alt='application for managing fleet vehicles'
-						/>
+						<a
+							href='https://warm-brook-93750-dev.herokuapp.com/'
+							target='blank'>
+							<img
+								src={require('./images/fleet.png')}
+								alt='application for managing fleet vehicles'
+							/>
+						</a>
 						<p className='desc'>
 							A full stack product that I collaborated with three other
 							engineers on. It is an application for companies to manage their

352f32e5b3e3c011ce0202ff92211e964e20329e
ClaytonC <cjcanady1113@gmail.com>
2020-11-15 13:17:11 -0500

    resume downloadable as a PDF

--- src/Resume.js
+++ src/Resume.js
@@ -1,17 +1,24 @@
+import { Button } from 'react-bootstrap';
 import React, { Component } from 'react';
+import myResume from './CJCresume.pdf';
 
 class Resume extends Component {
-    render() {
-        return (
-					<div>
-						<br/>
-						<img className='resume'
-							src={require('./images/CJCresume.png')}
-							alt='my resume'
-						/>
-					</div>
-				);
-    }
+	render() {
+		return (
+			<div>
+				<a href={myResume} download='./CJCresume.pdf'>
+					<Button className='my-3'>Download PDF</Button>
+				</a>
+				<br />
+				<img
+					className='resume'
+					src={require('./images/CJCresume.png')}
+					alt='my resume'
+				/>
+				<br />
+			</div>
+		);
+	}
 }
 
 export default Resume;
